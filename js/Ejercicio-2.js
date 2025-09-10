510b4e2f5248cdd054ff700776f0b9c1400e8a27
Matías Vivanco <matiasvivanco119@gmail.com>
2025-09-10 16:06:39 -0300

    Se resolvio el ejercicio 2  utilizando condicionales

---
+++ js/Ejercicio-2.js
@@ -0,0 +1,21 @@
+const nota = parseInt(prompt("Ingrese tu nota"));
+
+if (isNaN(nota)) {
+  alert(`Introduce un número valido`);
+} else if (nota >= 0 && nota <= 10) {
+  if (nota >= 0 && nota <= 2) {
+    alert("Muy deficiente");
+  } else if (nota >= 3 && nota <= 4) {
+    alert("Insuficiente");
+  } else if (nota >= 5 && nota <= 6) {
+    alert("Sufuciente");
+  } else if (nota == 7) {
+    alert("Bien");
+  } else if (nota >= 8 && nota <= 9) {
+    alert("Notable");
+  } else if (nota >= 10) {
+    alert("Sobresaliente");
+  }
+} else {
+  alert("Número erróneo");
+}
