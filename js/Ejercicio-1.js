4fc63e35933b3d79905d0631248d2a4d148c7970
Matías Vivanco <matiasvivanco119@gmail.com>
2025-09-10 15:50:47 -0300

    Se realizo una correcion en la linea 6 agregando un else if

--- js/Ejercicio-1.js
+++ js/Ejercicio-1.js
@@ -3,10 +3,8 @@ const edad = parseInt(prompt("Ingrese tu edad"));
 console.log(edad);
 if (isNaN(edad)) {
   document.writeln(`La edad no es valida`);
-} else {
-  if (edad >= 18) {
+} else if (edad >= 18) {
     document.writeln("Ya puedes conducir");
   } else {
     document.writeln("Falta unos años para poder conducir");
   }
-}
